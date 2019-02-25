cd098970c751e7b261b8f0cb363a277e42257597
abc <abc@example.com>
2019-02-25 11:19:01 +0700

    first

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var mysql =require('mysql');
+
+var app =express();
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+ var app = express();
+ app.listen("3000",console.log("server is runing at 3000"));
+
+ // setup public folder
+app.use(express.static("public"));
+
+// -------------setup routes-----------------
+var index = require("./routes/index.js")
+app.use("/",index);
+var loginPage = require('./routes/loginPage.js');
+app.use("/loginPage",loginPage);
+var ips = require('./routes/ips.js');
+app.use("/ips",ips);
+var err = require('./routes/404.js');
+app.use('*',err);
+
