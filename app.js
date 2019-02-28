dc5382c24b20910aceabd4d5759e96496d7a9fa2
abc <abc@example.com>
2019-02-28 22:45:14 +0700

    huynh

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 var express = require('express');
-var mysql =require('mysql');
+var app = express();
+var mysql = require('mysql');
+var server = require("http").Server(app);
+var io = require('socket.io')(server);
 
-var app =express();
+//create connection
+const db = mysql.createConnection({
+	host: "sql9.freemysqlhosting.net",
+	user: "sql9281167",
+	password: "QFpnvu4aCL",
+	database: 'sql9281167'
+});
+
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
- var app = express();
- app.listen(process.env.PORT||3000,console.log("server is runing at 3000"));
+//test database 
+io.on("connection",(socket)=>{
+	let sqlQuery = "SELECT * FROM posts";
+	db.query(sqlQuery,(err,result)=>{
+		if(err) throw err;
+		io.emit("database",result);
+	});
+});
 
- // setup public folder
+// setup public folder
 app.use(express.static("public"));
 
 // -------------setup routes-----------------
-var index = require("./routes/index.js")
-app.use("/",index);
+var index = require("./routes/index.js");
+app.use("/", index);
 var loginPage = require('./routes/loginPage.js');
-app.use("/loginPage",loginPage);
+app.use("/loginPage", loginPage);
 var ips = require('./routes/ips.js');
-app.use("/ips",ips);
-var err = require('./routes/404.js');
-app.use('*',err);
+app.use("/ips", ips);
+// const err = require('./routes/404.js');
+// app.use('*', err);
 
+server.listen(process.env.PORT || 3000, console.log("server is runing at 3000"));
