7c624a7ec55d058ba109beae60e1c6ef2720b1ab
abc <abc@example.com>
2019-03-30 08:56:06 +0700

    30/3

--- public/js/ips.js
+++ public/js/ips.js
@@ -1,43 +1,13 @@
 var a = new Image();
 var icon = new Image();
-function init(){
-  a.src = "img/110b3-3.png";
-  icon.src = "img/icon.png";
-  var canvas = document.getElementById("myCanvas");
-  var ctx = canvas.getContext("2d");
-  ctx.drawImage(a, 0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  function draw(){
-    $.ajax({
-      url: "../product",
-      method: "POST",
-      contentType: "application/json",
-      success: function(response) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(a, 0, 0, canvas.width, canvas.height);
-        ctx.arc(
-          (canvas.width * response.xcale) / 3,
-          (canvas.height * response.ycale) / 3,
-          6,
-          0,
-          2 * Math.PI
-        );
-        ctx.fillStyle = "red";
-        ctx.fill();
-      }
-    });
-  }
-  draw();
-}
-window.addEventListener('load',init,false);
-
-// $(document).ready(()=>{
-//   a.src = "img/110b3-2.png";
+// function init(){
+//   a.src = "img/110b3-3.png";
 //   icon.src = "img/icon.png";
 //   var canvas = document.getElementById("myCanvas");
 //   var ctx = canvas.getContext("2d");
 //   ctx.drawImage(a, 0, 0, canvas.width, canvas.height);
-//   setInterval(() => {
+//   ctx.beginPath();
+//   function draw(){
 //     $.ajax({
 //       url: "../product",
 //       method: "POST",
@@ -45,14 +15,44 @@ window.addEventListener('load',init,false);
 //       success: function(response) {
 //         ctx.clearRect(0, 0, canvas.width, canvas.height);
 //         ctx.drawImage(a, 0, 0, canvas.width, canvas.height);
-//         ctx.drawImage(
-//           icon,
+//         ctx.arc(
 //           (canvas.width * response.xcale) / 3,
 //           (canvas.height * response.ycale) / 3,
-//           30,
-//           30
+//           6,
+//           0,
+//           2 * Math.PI
 //         );
+//         ctx.fillStyle = "red";
+//         ctx.fill();
 //       }
 //     });
-//   }, 500);
-// });
+//   }
+//   draw();
+// }
+// window.addEventListener('load',init,false);
+
+$(document).ready(()=>{
+  a.src = "img/110b3-3.png";
+  icon.src = "img/icon.png";
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(a, 0, 0, canvas.width, canvas.height);
+  setInterval(() => {
+    $.ajax({
+      url: "../product",
+      method: "POST",
+      contentType: "application/json",
+      success: function(response) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(a, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(
+          icon,
+          (canvas.width * response.xcale) / 3,
+          (canvas.height * response.ycale) / 3,
+          30,
+          30
+        );
+      }
+    });
+  }, 50);
+});
