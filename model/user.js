b85ac21861b526d375b9098d5f50eee7e0c71842
huynhhoangst97 <44690128+huynhhoangst97@users.noreply.github.com>
2019-04-13 10:39:36 +0700

    Update user.js

--- model/user.js
+++ model/user.js
@@ -1,7 +1,25 @@
 var db = require('../connectMysql');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 var user = {
-    showUser: function (callback) {
-        return db.query("Select * from posts",callback);
+    showUser: (callback) => {
+        return db.query("Select * from user", callback);
+    },
+    insertUser: (username,password) => {
+        let post= {user:username, password:password};
+        let sql="INSERT INTO user SET ?";
+        db.query(sql,post,(err,result)=>{
+            if(err) throw err;
+        })
+    },
+    isMatchUser: (username, password, callback) =>{
+        let sql = "Select * from user Where user = ?";
+        db.query(sql,username,(err,data)=>{
+            var passwordDB=data[0]['password'];
+            return bcrypt.compare(password,passwordDB,callback);
+        });
+        
     }
 }
 module.exports = user;
