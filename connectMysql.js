b8bc34b031f7eb1255ec9d3a005177388c1bca0c
huynhoangst97 <huynhoang.st.97@gmail.com>
2019-05-04 23:50:50 +0700

    4/5

--- connectMysql.js
+++ connectMysql.js
@@ -1,8 +1,8 @@
 var mysql =require('mysql');
 var mysqlConnection = mysql.createConnection({
 	host: "sql9.freemysqlhosting.net",
-	user: "sql9283294",
-	password: "v7zJgtU9nY",
-	database: 'sql9283294'
+	user: "sql9290564",
+	password: "i4pic6jye5",
+	database: 'sql9290564'
 });
 module.exports= mysqlConnection;
