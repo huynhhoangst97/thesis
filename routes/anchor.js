791cb7449227b2952e656baae2dbb5982c29da5c
huynhoangst97 <huynhoang.st.97@gmail.com>
2019-06-14 10:04:34 +0700

    13/6

--- routes/anchor.js
+++ routes/anchor.js
@@ -5,16 +5,32 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var router=express.Router();
 var anchor  = require('../model/anchor');
 
-router.get("/5583",(req,res)=>{
-	anchor.findOneAndUpdate({tag:"5583"},{location:{xcale:req.query.longitude,ycale:req.query.latitude}},(err,result)=>{
+router.get("/0296",(req,res)=>{
+	anchor.findOneAndUpdate({tag:"0296"},{location:{xcale:req.query.longitude,ycale:req.query.latitude}},(err,result)=>{
 			if(err) throw err;
 			res.send("successful");
 	})
 });
+
 router.get("/8305",(req,res)=>{
 	anchor.findOneAndUpdate({tag:"8305"},{location:{xcale:req.query.longitude,ycale:req.query.latitude}},(err,result)=>{
 			if(err) throw err;
 			res.send("successful");
 	})
 });
+
+router.get("/0384",(req,res)=>{
+	anchor.findOneAndUpdate({tag:"0384"},{location:{xcale:req.query.longitude,ycale:req.query.latitude}},(err,result)=>{
+			if(err) throw err;
+			res.send("successful");
+	})
+});
+
+router.get("/4134",(req,res)=>{
+	anchor.findOneAndUpdate({tag:"4134"},{location:{xcale:req.query.longitude,ycale:req.query.latitude}},(err,result)=>{
+			if(err) throw err;
+			res.send("successful");
+	})
+});
+
 module.exports=router;
--- routes/product.js
+++ routes/product.js
@@ -9,36 +9,47 @@ var classGateway = require("../classGateway");
 
 var anchor_1 = new classLocation();
 var anchor_2 = new classLocation();
+var anchor_3 = new classLocation();
+var anchor_4 = new classLocation();
 var toaDo_1 = new classLocation();
 var toaDo_2 = new classLocation();
 var gateway_1 = new classGateway();
 
 router.post("/", (req, res) => {
-    location.find({ tag: "aa02" }, (err, result) => {
+    location.find({ tag: "152a" }, (err, result) => {
         toaDo_1.xcale = result[0]["location"]["xcale"];
         toaDo_1.ycale = result[0]["location"]["ycale"];
     });
-    location.find({ tag: "0296" }, (err, result) => {
+    location.find({ tag: "0c01" }, (err, result) => {
         toaDo_2.xcale = result[0]["location"]["xcale"];
         toaDo_2.ycale = result[0]["location"]["ycale"];
     });
 
     //anchor
-    anchor.find({ tag: "5583" }, (err, result) => {
+    anchor.find({ tag: "8305" }, (err, result) => {
         anchor_1.xcale = result[0]["location"]["xcale"];
         anchor_1.ycale = result[0]["location"]["ycale"];
     });
-    anchor.find({ tag: "8305" }, (err, result) => {
+    anchor.find({ tag: "0296" }, (err, result) => {
         anchor_2.xcale = result[0]["location"]["xcale"];
         anchor_2.ycale = result[0]["location"]["ycale"];
     });
+    anchor.find({ tag: "0384" }, (err, result) => {
+        anchor_3.xcale = result[0]["location"]["xcale"];
+        anchor_3.ycale = result[0]["location"]["ycale"];
+    });
+    anchor.find({ tag: "4134" }, (err, result) => {
+        anchor_4.xcale = result[0]["location"]["xcale"];
+        anchor_4.ycale = result[0]["location"]["ycale"];
+    });
+
     gateway.find({tag:"5322"},(err,result)=>{
         gateway_1.tag = result[0]['tag'];
         gateway_1.id = result[0]['id'];
     })
     // gateway 
     
-    res.send({ tag1_xcale: toaDo_1.xcale, tag1_ycale: toaDo_1.ycale, tag2_xcale: toaDo_2.xcale, tag2_ycale: toaDo_2.ycale, anchor1_xcale: anchor_1.xcale, anchor1_ycale: anchor_1.ycale, anchor2_xcale: anchor_2.xcale, anchor2_ycale: anchor_2.ycale, gateway1_tag: gateway_1.tag,gateway1_id : gateway_1.id })
+    res.send({ tag1_xcale: toaDo_1.xcale, tag1_ycale: toaDo_1.ycale, tag2_xcale: toaDo_2.xcale, tag2_ycale: toaDo_2.ycale, anchor1_xcale: anchor_1.xcale, anchor1_ycale: anchor_1.ycale, anchor2_xcale: anchor_2.xcale, anchor2_ycale: anchor_2.ycale, anchor3_xcale: anchor_3.xcale, anchor3_ycale: anchor_3.ycale, anchor4_xcale: anchor_4.xcale, anchor4_ycale: anchor_4.ycale , gateway1_tag: gateway_1.tag,gateway1_id : gateway_1.id })
     res.send("thanh cong");
 });
 
